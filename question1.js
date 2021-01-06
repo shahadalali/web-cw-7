0f57a4573030a8f528cfe98ad7a768968def5d18
Shahad Al Ali <76266035+shahadalali@users.noreply.github.com>
2021-01-06 10:38:18 +0300

    done

--- question1.js
+++ question1.js
@@ -1,1 +1,11 @@
-// قم بحل السؤال الأول هنا
+
+let p=prompt(`what are your favorite pets ?`)
+let pets= [`dog`,`bird`,`fish`,`hamster`,`turtle`];
+console.log(pets);
+pets.pop();
+console.log(pets);
+pets.push(`cat`);
+console.log(pets[4]);
+let pets1=prompt("change the name on index 2 ");
+pets.push(pets);
+console.log[pets];
